fbd573e7ed5aa3622033dd207b694391526b69a8
nosianuri <nurinosia@gmail.com>
2022-06-28 21:00:34 +0600

    added guiders

---
+++ src/Pages/home/Guiders/Gudiers.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import guider1 from '../../../images/guider/guide1.png';
+import guider2 from '../../../images/guider/guide2.png';
+import guider3 from '../../../images/guider/guide3.png';
+import guider4 from '../../../images/guider/guide4.png';
+import guider5 from '../../../images/guider/guide5.jpg';
+import guider6 from '../../../images/guider/guide6.jpg';
+import Gudier from '../Guider/Gudier';
+
+const guiders = [
+    {id:1, name: 'Munker Naim', img: guider1},
+    {id:2, name: 'Munker Naim', img: guider2},
+    {id:3, name: 'Munker Naim', img: guider3},
+    {id:4, name: 'Munker Naim', img: guider4},
+    {id:5, name: 'Munker Naim', img: guider5},
+    {id:6, name: 'Munker Naim', img: guider6},
+]
+
+const Gudiers = () => {
+    return (
+        <div className='container'>
+            <h2 className='text-primary text-center'>Our Touriest Guider</h2>
+            <div className='row'>
+                {
+                    guiders.map(guider => <Gudier
+                    key={guider.id}
+                    guider= {guider}
+                    ></Gudier>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Gudiers;
--- src/Pages/home/Home/Home.js
+++ src/Pages/home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Services from '../Services/Services';
+import Gudiers from '../Guiders/Gudiers';
 
 const Home = () => {
     return (
         <div>
             <Services></Services>
+            <Gudiers></Gudiers>
         </div>
     );
 };
