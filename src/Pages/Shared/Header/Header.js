2bee0ee92126ff6ffed3db39ffddf6760f50d6fd
nosianuri <nurinosia@gmail.com>
2022-09-11 02:53:59 +0600

    set search dates

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,7 +1,9 @@
 import { signOut } from 'firebase/auth';
 import React from 'react';
+// import SearchIcon from "@material-ui/icons/Search";
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import './Header.css';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import logo from '../../../images/logo.png';
@@ -23,13 +25,17 @@ const Header = () => {
               <Nav.Link href="home">Home</Nav.Link>
               <Nav.Link href="home#services">Services</Nav.Link>
               <Nav.Link href="home#guiders">Guiders</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <div className='header__center'>
+                <input type="text" />
+                <svg width={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>
+            </div>
+              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
             <Nav>
               <Nav.Link as={Link} to="about">About</Nav.Link>
--- src/Pages/home/Banner/Banner.js
+++ src/Pages/home/Banner/Banner.js
@@ -1,30 +1,50 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
-
+import { Button } from "@material-ui/core";
+import Search from './Search';
+import './Banner.css';
 import banner1 from '../../../images/Rectangle 1.png';
 import banner2 from '../../../images/Rectangle 28.png';
 import banner3 from '../../../images/Rectangle 27.png';
+import { useNavigate } from 'react-router-dom';
 
 const Banner = () => {
-    const [index, setIndex] = useState(0);
+  const navigate = useNavigate();
+  const [index, setIndex] = useState(0);
+  const [showSearch, setShowSearch] = useState(false);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
-    return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
+        <div className='banner__search'>
+          {showSearch && <Search />}
+
+          <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
+            {showSearch ? "Hide" : "Search Dates"}
+          </Button>
+        </div>
         <img
           className="d-block w-100" height={"600"}
           src={banner1}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>Welcome to Bangladesh Tour</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3 className="banner-text">Welcome to Bangladesh Tour</h3>
+          <p className="banner-text">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Button className='ENbtn' onClick={() => navigate.push('/search')} variant='outlined'>Explore Nearby</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <div className='banner__search'>
+          {showSearch && <Search />}
+
+          <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
+            {showSearch ? "Hide" : "Search Dates"}
+          </Button>
+        </div>
         <img
           className="d-block w-100" height={"600"}
           src={banner2}
@@ -32,11 +52,19 @@ const Banner = () => {
         />
 
         <Carousel.Caption>
-          <h3>Adventure Is Worthwhile </h3>
-          <p>DISCOVER NEW PLACES WITH BD, TRAVEL GURU.</p>
+          <h3 className="banner-text">Adventure Is Worthwhile </h3>
+          <p className="banner-text">DISCOVER NEW PLACES WITH BD, TRAVEL GURU.</p>
+          <Button className='ENbtn' onClick={() => navigate.push('/search')}variant='outlined'>Explore Nearby</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <div className='banner__search'>
+          {showSearch && <Search />}
+
+          <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
+            {showSearch ? "Hide" : "Search Dates"}
+          </Button>
+        </div>
         <img
           className="d-block w-100" height={"600"}
           src={banner3}
@@ -44,14 +72,15 @@ const Banner = () => {
         />
 
         <Carousel.Caption>
-          <h3>Incredible last-minute hotel deals</h3>
-          <p>
+          <h3 className="banner-text">Incredible last-minute hotel deals</h3>
+          <p className="banner-text">
             SAVE MONEY AND TIME BY BOOKING TONIGHT AND BEYOND.
           </p>
+          <Button className='ENbtn' onClick={() => navigate.push('/search')}variant='outlined'>Explore Nearby</Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    );
+  );
 };
 
 export default Banner;
